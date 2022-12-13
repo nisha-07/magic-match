02d3773749e22f6d6231208b787b45783ae0b0c0
nisha-07 <nishasataki@gmail.com>
2022-12-13 11:22:20 +0530

    Magic match: Check whether two cards are equal or not

--- magic-match/src/App.js
+++ magic-match/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 import SingleCard from './components/SingleCard/SingleCard';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const cardImages = [
   { src: "img/rose.png" },
@@ -16,23 +16,47 @@ function App() {
 
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages].sort(() => Math.random() - 0.5).map((card) => ({ ...card, id: Math.random() }));
 
     setCards(shuffledCards);
     setTurns(0);
-    console.log(turns, cards);
   }
 
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+    // we cannot check here whether the two choices are equal or not
+    // because it will be checked before updating the states
+  }
 
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        console.log("those cards are equal")
+        resetTurn();
+      }
+      else {
+        console.log("those cards are not equal")
+        resetTurn();
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns(turns + 1);
+  }
   return (
     <div className="App">
       <h1> Magic match</h1 >
       <button onClick={shuffleCards}>New game</button>
       <div className="card-grid" >
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card} />
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
         ))}
       </div>
     </div>
--- magic-match/src/components/SingleCard/SingleCard.js
+++ magic-match/src/components/SingleCard/SingleCard.js
@@ -1,11 +1,19 @@
 import classes from "./SingleCard.module.css"
 
-const SingleCard = ({ card }) => {
+const SingleCard = ({ card, handleChoice }) => {
+
+    const handleClick = () => {
+        handleChoice(card);
+    }
 
     return (
         <div className={classes.card}>
             <img className={classes.front} src={card.src} alt="card front" />
-            <img className={classes.back} src="/img/cover.png" alt="cover" />
+            <img
+                className={classes.back}
+                src="/img/cover.png"
+                onClick={handleClick}
+                alt="cover" />
         </div>
 
     )
